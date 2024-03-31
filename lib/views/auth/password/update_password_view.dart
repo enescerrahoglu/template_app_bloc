1798b223cf743b69392016fd9c8f1ebb0d754642
enescerrahoglu <enescerrahoglu1@gmail.com>
2024-03-31 17:04:44 +0300

    update_password_view fixed

--- lib/views/auth/password/update_password_view.dart
+++ lib/views/auth/password/update_password_view.dart
@@ -39,47 +39,50 @@ class _UpdatePasswordViewState extends State<UpdatePasswordView> with UpdatePass
                   listener: (context, state) {
                     _listener(state);
                   },
-                  child: CustomScaffold(
-                    title: LocaleKeys.new_password,
-                    trailing: CustomTrailing(
-                      text: LocaleKeys.cancel,
-                      isLoading: loginState.isLoading,
-                      onPressed: () {
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) => const LoginView(),
-                          ),
-                          (route) => false,
-                        );
-                      },
-                    ),
-                    children: [
-                      const Text(LocaleKeys.enter_valid_password).tr(),
-                      const SizedBox(height: 10),
-                      CustomTextField(
-                        textEditingController: _newPasswordTextEditingController,
-                        prefixIcon: CupertinoIcons.lock,
-                        placeholder: LocaleKeys.password.tr(),
-                        obscureText: true,
-                        textInputAction: TextInputAction.done,
-                        enabled: !loginState.isLoading,
-                        onSubmitted: (loginState.isLoading || registerState.message == null)
-                            ? null
-                            : (p0) {
-                                _updateButtonOnPressed(loginBloc: loginBloc, registerState: registerState);
-                              },
-                      ),
-                      const SizedBox(height: 10),
-                      UpdatePasswordButton(
+                  child: PopScope(
+                    canPop: !loginState.isLoading,
+                    child: CustomScaffold(
+                      title: LocaleKeys.new_password,
+                      trailing: CustomTrailing(
+                        text: LocaleKeys.cancel,
                         isLoading: loginState.isLoading,
-                        onPressed: (loginState.isLoading || registerState.message == null)
-                            ? null
-                            : () {
-                                _updateButtonOnPressed(loginBloc: loginBloc, registerState: registerState);
-                              },
+                        onPressed: () {
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            CupertinoPageRoute(
+                              builder: (context) => const LoginView(),
+                            ),
+                            (route) => false,
+                          );
+                        },
                       ),
-                    ],
+                      children: [
+                        const Text(LocaleKeys.enter_valid_password).tr(),
+                        const SizedBox(height: 10),
+                        CustomTextField(
+                          textEditingController: _newPasswordTextEditingController,
+                          prefixIcon: CupertinoIcons.lock,
+                          placeholder: LocaleKeys.password.tr(),
+                          obscureText: true,
+                          textInputAction: TextInputAction.done,
+                          enabled: !loginState.isLoading,
+                          onSubmitted: (loginState.isLoading || registerState.message == null)
+                              ? null
+                              : (p0) {
+                                  _updateButtonOnPressed(loginBloc: loginBloc, registerState: registerState);
+                                },
+                        ),
+                        const SizedBox(height: 10),
+                        UpdatePasswordButton(
+                          isLoading: loginState.isLoading,
+                          onPressed: (loginState.isLoading || registerState.message == null)
+                              ? null
+                              : () {
+                                  _updateButtonOnPressed(loginBloc: loginBloc, registerState: registerState);
+                                },
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
